c59883026221763f2a52b62fb09e1c25f00bc6b6
Lee-SeungBin <tmdqlszld123@naver.com>
2023-12-20 22:23:14 +0900

    jenkins 설정

---
+++ Assets/Editer/BuildPlayer.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildPlayer : MonoBehaviour
+{
+    [MenuItem("Build/Build AOS")]
+    public static void MyBuild_AOS()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/GameScene2.unity" };
+        buildPlayerOptions.locationPathName = $"Builds/AOS_{PlayerSettings.bundleVersion}.apk";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
